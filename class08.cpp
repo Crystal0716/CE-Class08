34bd1d631b9d9833840d64fb1c0fa7c83f6c4aba
若水 <168269104+Crystal0716@users.noreply.github.com>
2024-06-13 20:37:23 +0800

    Add files via upload

---
+++ class08.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std ;
+#define k
+int main ()
+{
+char a , b ;
+cin >> a;
+b = a | 32 ;
+cout <<"Enter a character : "<< a << endl << b ;
+return (0);
+}
